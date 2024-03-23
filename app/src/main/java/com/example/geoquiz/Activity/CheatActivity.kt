80b25f86f2ade61d505b840e6db120c9a5b3aaa0
Priya Boopathi <priyanandh06@gmail.com>
2024-03-23 16:19:57 -0700

    Added support for OnBackPressedCallback

--- app/src/main/java/com/example/geoquiz/Activity/CheatActivity.kt
+++ app/src/main/java/com/example/geoquiz/Activity/CheatActivity.kt
@@ -2,11 +2,31 @@ package com.example.geoquiz.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import com.example.geoquiz.R
+import com.example.geoquiz.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
+
+    private lateinit var cheatActivityBinding: ActivityCheatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cheat)
+        setContentView(cheatActivityBinding.root)
+        onBackPressedDispatcher.addCallback(this, callBack)
+
     }
+
+
+    private val callBack = object : OnBackPressedCallback(true) {
+        /**
+         * Callback for handling the [OnBackPressedDispatcher.onBackPressed] event.
+         */
+        override fun handleOnBackPressed() {
+            finish()
+        }
+
+    }
+
+
 }
